a078260a560b7cff148466df0a0e8aef0b945263
Guilherme Vale <guilherme.vale98@gmail.com>
2024-12-12 08:48:00 -0300

    add services animation

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,23 +1,32 @@
-import { abril, inter, Service } from '@/app/constants'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import { abril, inter, Service } from "@/app/constants";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 type Props = {
-    service: Service
-}
+    service: Service;
+};
 
 const ServiceCard = ({ service }: Props) => {
     return (
-        <div
+        <motion.div
             key={service.id}
-            className="border border-themeYellow md:w-[45%] p-8   flex flex-col items-center text-center"
+            className="border border-themeYellow md:w-[45%] p-8 flex flex-col items-center text-center"
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }} 
+            viewport={{ once: true, amount: 0.2 }} 
+            transition={{
+                duration: 0.5,
+                delay: service.id * 0.2, 
+                ease: "easeOut",
+            }}
         >
-            <div className=" h-[100px] flex items-center flex-col justify-end overflow-hidden mb-6">
+            <div className="h-[100px] flex items-center flex-col justify-end overflow-hidden mb-6">
                 <Image
                     alt={service.alt}
                     src={service.iconUrl}
-
-                    className='object-contain max-h-[80px]'                />
+                    className="object-contain max-h-[80px]"
+                />
             </div>
             <h3 className={`${abril.className} text-[20px] text-white`}>
                 {service.title}
@@ -25,8 +34,8 @@ const ServiceCard = ({ service }: Props) => {
             <p className={`${inter.className} text-[14px] text-gray-300 mt-2`}>
                 {service.description}
             </p>
-        </div>
-    )
-}
+        </motion.div>
+    );
+};
 
-export default ServiceCard
+export default ServiceCard;
